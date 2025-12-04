25098c0eb75229795fa5a9f3e4173c40af0f5ffe
0xsomejs <somejs@proton.me>
2025-12-04 17:35:44 +0100

    Create exc.hpp

---
+++ exc.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+
+namespace Offsets {
+
+	inline constexpr uintptr_t EnableLoadModule = 0x67333c8;
+
+	inline constexpr uintptr_t FakeDataModelPointer = 0x78a24c8;
+	inline constexpr uintptr_t FakeDataModelToDataModel = 0x1C0;
+
+	inline constexpr uintptr_t Children = 0x70;
+	inline constexpr uintptr_t ChildrenEnd = 0x8;
+	inline constexpr uintptr_t Name = 0xB0;
+	inline constexpr uintptr_t Value = 0xD0;
+
+	inline constexpr uintptr_t ClassDescriptor = 0x18;
+	inline constexpr uintptr_t ClassDescriptorToClassName = 0x8;
+
+	inline constexpr uintptr_t LocalScriptByteCode = 0x1A8;
+	inline constexpr uintptr_t ModuleScriptByteCode = 0x150;
+}
