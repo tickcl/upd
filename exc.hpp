f339085d4a026ac7c7202406be26c5bbe3d6d1d2
0xsomejs <somejs@proton.me>
2025-12-09 15:21:07 +0100

    Update exc.hpp

--- exc.hpp
+++ exc.hpp
@@ -2,17 +2,17 @@
 #include <Windows.h>
 
 namespace Offsets {
-	inline constexpr uintptr_t EnableLoadModule = 0x67333c8;
-
-	inline constexpr uintptr_t FakeDataModelPointer = 0x78a24c8;
+	inline constexpr uintptr_t EnableLoadModule = 0x679c8f8;
+	
+	inline constexpr uintptr_t FakeDataModelPointer = 0x7d02458;
 	inline constexpr uintptr_t FakeDataModelToDataModel = 0x1C0;
-
+	
 	inline constexpr uintptr_t Children = 0x70;
 	inline constexpr uintptr_t ChildrenEnd = 0x8;
 	inline constexpr uintptr_t Name = 0xB0;
-
+	
 	inline constexpr uintptr_t Value = 0xD0;
-
+	
 	inline constexpr uintptr_t ClassDescriptor = 0x18;
 	inline constexpr uintptr_t ClassDescriptorToClassName = 0x8;
 
